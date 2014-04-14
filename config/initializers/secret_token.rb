01fed8fce15e6c01dee9875d29a082b0c729817c
Patti Jurgensen <patricia.jurgensen@gmail.com>
2014-04-13 23:36:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GardenTracker::Application.config.secret_key_base = 'cd1e706a65ff1872097702de74e8ecef7989579445e9281b930e904576d1806f9c20ce292a43385fb8a3dedf83a0e9a67ad24e874e8dcc57ac953159ad96c48a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GardenTracker::Application.config.session_store :cookie_store, key: '_garden_tracker_session'
