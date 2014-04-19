cbcece4c1604e2b657f2ee07205fa68230b9f33e
Patti Jurgensen <patricia.jurgensen@gmail.com>
2014-04-19 16:58:39 -0700

    add ability to create follow up task when adding a garden task and order garden tasks by date

--- app/controllers/garden_tasks_controller.rb
+++ app/controllers/garden_tasks_controller.rb
@@ -1,6 +1,9 @@
 class GardenTasksController < ApplicationController
 	def index
 		@garden_tasks = GardenTask.all
+		@today_tasks = GardenTask.where(date: Date.today)
+		@future_tasks = GardenTask.where(['date > ?', Date.today]).order('date DESC')
+		@past_tasks = GardenTask.where(['date < ?', Date.today]).order('date DESC')
 	end
 
 	def new
@@ -9,7 +12,18 @@ class GardenTasksController < ApplicationController
 
 	def create
 		@garden_task = GardenTask.create(garden_task_params)
-		if @garden_task.save
+
+		if params[:days_away] && params[:future_description] != nil
+			days_away = params[:days_away]
+			future_date = GardenTask.new_date(params[:garden_task][:date], days_away)
+			puts "Future date: " + future_date
+			@future_task = GardenTask.create(date: future_date, description: params[:future_description])
+			future_task_okay = true
+		else
+			future_task_okay = true
+		end
+
+		if @garden_task.save && future_task_okay
 			flash[:notice] = "Your task was created"
 			redirect_to garden_tasks_path
 		else
@@ -21,6 +35,6 @@ class GardenTasksController < ApplicationController
 private
 
   def garden_task_params
-    params.require(:garden_task).permit(:description, :date)
+    params.require(:garden_task).permit(:date, :description)
   end
 end
--- app/models/garden_task.rb
+++ app/models/garden_task.rb
@@ -2,4 +2,9 @@ class GardenTask < ActiveRecord::Base
 	validates :description, presence: true
 	validates :date, presence: true
 
+	def self.new_date(original_date, days_away)
+		date = original_date.to_date
+	  (date + days_away.to_i).to_s
+	end
+
 end
