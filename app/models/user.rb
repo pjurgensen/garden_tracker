351f07e13ca0caae6dae63c9ccfc81ff7ec34848
Patti Jurgensen <student@epicodus.com>
2014-05-09 11:44:27 -0700

    add validation of username presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true
 
-  # validates :username, presence: true
-  # valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email,
-  #           presence: true,
-  #           format: { with: valid_email_regex },
-  #           uniqueness: true,
-  #           case_sensitive: false
-  # validates :password, presence: true, length: { minimum: 6 }
-  # validates :password_digest, presence: true, uniqueness: true
 end
